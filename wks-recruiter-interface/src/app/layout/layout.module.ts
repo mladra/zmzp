08fc759c4cb1745ea7c17931d3ae4013db3b6c42
Jakub Krysiak <195634@edu.p.lodz.pl>
2018-04-05 18:53:03 +0200

    [ZMZPWYB-38] Dodano pusty komponent zarządzania testami.

--- wks-recruiter-interface/src/app/layout/layout.module.ts
+++ wks-recruiter-interface/src/app/layout/layout.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgbDropdownModule, NgbAlertModule, NgbModal, NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AccountsModule } from './accounts/accounts.module';
+import { TestsModule } from './tests/tests.module';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
@@ -18,7 +19,8 @@ import { AlertsService } from '../services/alerts.service';
         AccountsModule,
         NgbAlertModule.forRoot(),
         NgbDropdownModule.forRoot(),
-        NgbModule.forRoot()
+        NgbModule.forRoot(),
+        TestsModule
     ],
     declarations: [LayoutComponent, SidebarComponent, HeaderComponent],
     providers: [AlertsService]
