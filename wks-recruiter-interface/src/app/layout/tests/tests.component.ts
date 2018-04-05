c763248cd004391154eab65a7ba71dd8bd88526d
Jakub Krysiak <195634@edu.p.lodz.pl>
2018-04-05 20:45:10 +0200

    [ZMZPWYB-38] Naprawa importów i deklaracji.

--- wks-recruiter-interface/src/app/layout/tests/tests.component.ts
+++ wks-recruiter-interface/src/app/layout/tests/tests.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from "../../router.animations";
 
 @Component({
   selector: 'app-tests',
   templateUrl: './tests.component.html',
-  styleUrls: ['./tests.component.scss']
+  styleUrls: ['./tests.component.scss'],
+  animations: [routerTransition()]
 })
 export class TestsComponent implements OnInit {
 
--- wks-recruiter-interface/src/app/layout/tests/tests.module.ts
+++ wks-recruiter-interface/src/app/layout/tests/tests.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { TestsComponent } from "./tests.component";
 import { TestsRoutingModule } from './tests-routing.module';
 import { TestsListComponent } from './tests-list/tests-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    TestsRoutingModule
+    TestsRoutingModule,
+    CommonModule
   ],
-  declarations: [TestsListComponent]
+  declarations: [TestsComponent, TestsListComponent]
 })
 export class TestsModule { }
