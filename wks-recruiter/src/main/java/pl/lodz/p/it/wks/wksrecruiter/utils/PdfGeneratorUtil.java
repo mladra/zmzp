212698634e08808f2418a46669ac560b3164c19d
nullJaX <195643@edu.p.lodz.pl>
2018-04-11 11:29:40 +0200

    [ZMZPWYB-56] #time 3h #comment Ukończono tworzenie szablonu dokumentu PDF

--- wks-recruiter/src/main/java/pl/lodz/p/it/wks/wksrecruiter/utils/PdfGeneratorUtil.java
+++ wks-recruiter/src/main/java/pl/lodz/p/it/wks/wksrecruiter/utils/PdfGeneratorUtil.java
@@ -1,21 +1,7 @@
 package pl.lodz.p.it.wks.wksrecruiter.utils;
 
-import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.DocumentFont;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.tool.xml.XMLWorker;
-import com.itextpdf.tool.xml.XMLWorkerHelper;
-import com.itextpdf.tool.xml.css.CssFile;
-import com.itextpdf.tool.xml.css.StyleAttrCSSResolver;
-import com.itextpdf.tool.xml.html.Tags;
-import com.itextpdf.tool.xml.parser.XMLParser;
-import com.itextpdf.tool.xml.pipeline.css.CSSResolver;
-import com.itextpdf.tool.xml.pipeline.css.CssResolverPipeline;
-import com.itextpdf.tool.xml.pipeline.end.PdfWriterPipeline;
-import com.itextpdf.tool.xml.pipeline.html.HtmlPipeline;
-import com.itextpdf.tool.xml.pipeline.html.HtmlPipelineContext;
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -23,12 +9,10 @@ import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.xhtmlrenderer.pdf.ITextFontResolver;
-import org.xhtmlrenderer.pdf.ITextOutputDevice;
 import org.xhtmlrenderer.pdf.ITextRenderer;
-import org.xhtmlrenderer.pdf.ITextUserAgent;
 import pl.lodz.p.it.wks.wksrecruiter.collections.Test;
 
-import java.io.*;
+import java.io.IOException;
 
 @Component
 public class PdfGeneratorUtil {
