346c8ee9a6762dd65b9b4b3f348330cfd126b305
nullJaX <195643@edu.p.lodz.pl>
2018-05-11 03:25:49 +0200

    [ZMZPWYB-62] #time 1h 10m #comment Poprawa komponentu wysyłającego wiadomość email

--- wks-recruiter/src/main/java/pl/lodz/p/it/wks/wksrecruiter/services/MailService.java
+++ wks-recruiter/src/main/java/pl/lodz/p/it/wks/wksrecruiter/services/MailService.java
@@ -1,39 +1,38 @@
 package pl.lodz.p.it.wks.wksrecruiter.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 import pl.lodz.p.it.wks.wksrecruiter.collections.TestAttempt;
-import pl.lodz.p.it.wks.wksrecruiter.exceptions.WKSRecruiterException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class MailService {
+	@Autowired
 	private JavaMailSender mailSender;
 	
 	@Autowired
-	private MailBuilder mailBuilder;
+	private Environment env;
 	
 	@Autowired
-	public MailService(JavaMailSender javaMailSender) {
-		this.mailSender = javaMailSender;
-	}
+	private TemplateEngine templateEngine;
 	
-	public void sendEmail(String emailAddress, TestAttempt testAttempt) throws WKSRecruiterException {
-		MimeMessagePreparator messagePreparator = mimeMessage -> {
-			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
-			messageHelper.setFrom("sample@dolszewski.com");
-			messageHelper.setTo(emailAddress);
-			messageHelper.setSubject("Sample mail subject");
-			String content = mailBuilder.buildMail(testAttempt);
-			messageHelper.setText(content, true);
-		};
-		try {
-			mailSender.send(messagePreparator);
-		} catch (MailException e) {
-			throw WKSRecruiterException.of(e);
-		}
+	public void sendEmail(String emailAddress, TestAttempt testAttempt) throws MessagingException {
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true);
+		helper.setFrom(env.getProperty("spring.mail.username"));
+		helper.setTo(emailAddress);
+		helper.setSubject("WKS-Recruiter: Results");
+		Context ctx = new Context();
+		ctx.setVariable("test", testAttempt);
+		String content = templateEngine.process("mailTemplate",ctx);
+		helper.setText(content, true);
+		mailSender.send(message);
 	}
 }
