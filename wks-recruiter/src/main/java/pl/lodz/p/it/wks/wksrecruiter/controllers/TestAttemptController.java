07ce8838c0abe3291d513ddaf43addbf66dfddc3
DropsOnly <michaldropsladra@gmail.com>
2018-05-11 18:04:28 +0200

    [ZMZPWYB-65] #comment Implementacja backendu do oceniania testu. #time 1h30m

--- wks-recruiter/src/main/java/pl/lodz/p/it/wks/wksrecruiter/controllers/TestAttemptController.java
+++ wks-recruiter/src/main/java/pl/lodz/p/it/wks/wksrecruiter/controllers/TestAttemptController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pl.lodz.p.it.wks.wksrecruiter.collections.TestAttempt;
 import pl.lodz.p.it.wks.wksrecruiter.exceptions.WKSRecruiterException;
 import pl.lodz.p.it.wks.wksrecruiter.services.TestAttemptService;
 
@@ -14,7 +16,6 @@ import pl.lodz.p.it.wks.wksrecruiter.services.TestAttemptService;
 @RequestMapping(value = "/test-attempts")
 public class TestAttemptController {
 
-    @Autowired
     private final TestAttemptService testAttemptService;
 
     @Autowired
@@ -28,6 +29,25 @@ public class TestAttemptController {
             return ResponseEntity.ok(this.testAttemptService.getTestAttempts(authentication));
         } catch (WKSRecruiterException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(e.toString());
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/all")
+    public ResponseEntity getAllTestsAttempts(Authentication authentication) {
+        try {
+            return ResponseEntity.ok(this.testAttemptService.getAllTestsAttempts(authentication));
+        } catch (WKSRecruiterException exc) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(exc.toString());
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity evaluateTestAttempt(@RequestBody TestAttempt testAttempt, Authentication authentication) {
+        try {
+            testAttemptService.evaluateTestAttempt(testAttempt, authentication);
+            return ResponseEntity.ok().build();
+        } catch (WKSRecruiterException exc) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(exc.toString());
         }
     }
 }
